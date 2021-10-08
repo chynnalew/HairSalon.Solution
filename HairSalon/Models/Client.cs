ee745679cfc7100cfeb753106324617ff2785eb4
chynnalew <chynnalew@yahoo.com>
2021-10-08 09:30:54 -0700

    add client class, rename database

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,6 +2,10 @@ namespace HairSalon.Models
 {
   public class Client
   {
-
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public string Notes {get; set;}
+    public int StylistId {get; set;}
+    public virtual Stylist Stylist {get; set;}
   }
 }
